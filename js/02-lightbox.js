c6e8084adb5147f1c965aec6df0e252aaf150043
IngaPirogova <ingeborga.pirogova@gmail.com>
2022-10-11 12:07:44 +0300

    Update 02-js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,42 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-console.log(galleryItems);
+// // Change code below this line
 
 
 const divGallery = document.querySelector(".gallery");
+ const markup =  makeGallery(galleryItems);
+
+
+divGallery.insertAdjacentHTML("afterbegin", markup);
 
 function  makeGallery(galleryItems) {
 	return galleryItems
 		.map(({ preview, original, description }) => {
-			return `
-        <li>
-<a class="gallery__item" href="${original}">
-    <img
-    class="gallery__image"
-    src="${preview}"
-    alt="${description}"
-    data-source="${original}"
-    />
-</a>
-</li>
-`;
-		})
-		.join("");
+			return `<li>
+                  <a class="gallery__item" href="${original}">
+                     <img
+                       class="gallery__image"
+                       src="${preview}"
+                       alt="${description}"   
+                       />
+                       </a>
+                       </li> `;  })	.join("");
 }
-
-const markup =  makeGallery(galleryItems);
-divGallery.insertAdjacentHTML("afterbegin", markup);
-
-divGallery.addEventListener('click', onItemsClick);
-function onItemsClick(e) {
-    e.preventDefault();
-    const imageEl = e.target.classList.contains("gallery__image")
-    console.log(imageEl)
-    if (!imageEl) {
-        return;
-    }
+ 
     const lightbox = new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250, });
 
-};
 
- 
+
